45b2d331a9c7878214b77a1eac2b31410d083cd5
Jehart Bonilla <bonillajehart@gmail.com>
2019-09-23 20:44:01 +0800

    finalized deployment

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t seriousbob/multi-client:latest -t seriousbob/multi-client:$SHA  -f ./client/Dockerfile.dev ./client
+docker build -t seriousbob/multi-server:latest -t seriousbob/multi-server:$SHA -f ./server/Dockerfile.dev ./server
+docker build -t seriousbob/multi-worker:latest -t seriousbob/multi-worker:$SHA -f ./worker/Dockerfile.dev ./worker
+
+docker push seriousbob/multi-client:latest
+docker push seriousbob/multi-client:$SHA
+
+docker push seriousbob/multi-server:latest
+docker push seriousbob/multi-server:$SHA
+
+docker push seriousbob/multi-worker:latest
+docker push seriousbob/multi-worker:$SHA
+
+kubectl apply -f k8
+kubectl set image deployments/server-deployment server=seriousbob/multi-server:$SHA
+kubectl set image deployments/client-deployment server=seriousbob/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=seriousbob/multi-worker:$SHA
